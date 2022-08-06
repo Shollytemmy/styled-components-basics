f5739d8d9e4f8b7f6b62a26e27ba37c6166c09a3
Bunyameen <bunyameen@gmail.com>
2022-08-06 07:43:06 +0100

    create Title and Wrapper components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-
+import styled from 'styled-components';
 import './App.css';
+
+const Title = styled.h1`
+font-color: 1.5em;
+text-align: center;
+color: teal;
+`;
+
+const Wrapper = styled.section`
+padding: 4em;
+background-color: #e5d9c5;
+
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Styled Components Basics</h1>
+    <Wrapper>
+      <Title>
+        Styled Components Basics
+
+      </Title>
+      
     
-    </div>
+    </Wrapper>
   );
 }
 
