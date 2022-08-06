34613c3e86c290b22c9b4e4707154955a6cd2db4
Bunyameen <bunyameen@gmail.com>
2022-08-06 09:01:44 +0100

    add different button component

--- src/App.js
+++ src/App.js
@@ -5,30 +5,65 @@ const Title = styled.h1`
 font-color: 1.5em;
 text-align: center;
 color: teal;
+border:  0px solid red;
 `;
+
+
 
 const Wrapper = styled.section`
 padding: 4em;
 background-color: #e5d9c5;
 height: 100vh;
+position: relative;
 
 `;
 
 const Button = styled.button`
 background-color: ${((props) => props.backgroundColor ==='red' ? 'red' : 'teal')};
-color: ${(props => props.color ==='green' ? 'green' : 'white')}
+color: ${(props => props.color ==='green' ? 'green' : 'white')};
+padding: 10px 15px;
+border: none;
+margin: 5rem;
+cursor: pointer;
+border-radius: 5px;
+font-size: 18px;
+font-weight: lighter;
+:hover{
+  background: #000;
+};
 
 `;
+
+const Button1 = styled.button`
+background: pink;
+font-size: 18px;
+padding: 10px 15px;
+color: #000;
+border: none;
+border-radius: 5px;
+cursor: pointer;
+font-weight: lighter;
+&:hover{
+  background: #000;
+  color: #fff;
+}
+
+` ;
+
+
 
 function App() {
   return (
     <Wrapper>
       <Title>
         Styled Components Basics
-        <Button backgroundColor='red' color='green'>Add to cart</Button>
-        <Button backgroundColor='teal' color='white'>Subscribe</Button>
         
       </Title>
+
+      <Button backgroundColor='red' color='green'>Add to cart</Button>
+        <Button backgroundColor='teal' color='white'>Subscribe</Button>
+
+        <Button1>Plain button</Button1>
     </Wrapper>
     
   );
