8996c6bab137dfffa89ea1222e5c6e5e6a082d89
Bunyameen <bunyameen@gmail.com>
2022-08-06 08:09:16 +0100

    Button components added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ color: teal;
 const Wrapper = styled.section`
 padding: 4em;
 background-color: #e5d9c5;
+height: 100vh;
+
+`;
+
+const Button = styled.button`
+background-color: ${((props) => props.backgroundColor ==='red' ? 'red' : 'teal')};
+color: ${(props => props.color ==='green' ? 'green' : 'white')}
 
 `;
 
@@ -18,11 +25,12 @@ function App() {
     <Wrapper>
       <Title>
         Styled Components Basics
-
+        <Button backgroundColor='red' color='green'>Add to cart</Button>
+        <Button backgroundColor='teal' color='white'>Subscribe</Button>
+        
       </Title>
-      
-    
     </Wrapper>
+    
   );
 }
 
